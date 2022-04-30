d9da89987a851271a7dcf6946531662130cd678f
Darshan <darshankharche@gmail.com>
2022-04-30 12:16:05 +0530

    updated personal details

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,12 @@ export default function Footer(){
     return(
         <div className='footer'>
             <hr></hr>
-            <a href="mailto:mohitmetkar2317@gmail.com">mohitmetkar2317@gmail.com</a>
+            <a href="mailto:vistar.architect23@gmail.com">vistar.architect23@gmail.com</a>
             <div className='all-links'>
                 <a href="https://www.instagram.com/vistar_architects" >
                     <i className="fa-brands fa-instagram"></i>
                 </a>  
-                <a href="https://wa.me/919511723508" >
+                <a href="https://wa.me/918446465019" >
                     <i className="fa-brands fa-whatsapp"></i>
                 </a>
             </div>
